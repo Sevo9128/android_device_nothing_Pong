2186bf53f394ac23e29df1cb1b5682d42bcfb8f3
Sevo9128 <sevo9128@gmail.com>
2026-02-08 13:30:12 +0100

    Avium specifics

--- lineage_Pong.mk
+++ lineage_Pong.mk
@@ -31,3 +31,41 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=Pong \
     SystemDevice=Pong \
     SystemName=Pong
+
+# AVIUM_VERSION_APPEND_TIME_OF_DAY is a boolean flag to indicate
+# whether to append time of day to the build date.
+AVIUM_VERSION_APPEND_TIME_OF_DAY ?= true
+# Maintainer
+# AVIUM_MAINTAINER is a string that represents the maintainer of the build.
+AVIUM_MAINTAINER ?= Sevo9128
+
+# Settings
+# Soc model name
+AVIUM_SETTINGS_SOC_MODEL_NAME ?= Snapdragon_8+_Gen_1
+# Device code name
+AVIUM_SETTINGS_DEVICE_CODENAME ?= Pong
+
+# GMS
+# WITH_GMS is a boolean flag to indicate 
+# whether to include Google Mobile Services (GMS) in the build.
+WITH_GMS ?= false
+# Google Sans
+# Enable this to set default fonts to Google Sans.
+# This also works when building vanilla, but you
+# need to sync vendor/pixel/gsans repo.
+TARGET_USES_GSANS := false
+# LatinIMEGooglePrebuilt
+# Only works on vanilla builds,
+# GMS builds will use the Google IME from GMS.
+TARGET_INCLUDE_GOOGLEIME ?= false
+TARGET_GOOGLEIME_OVERRIDE_IME ?= false
+
+# Spoof Props
+# Set to true to enable spoofing fake props.
+# For letting apps think they are running on a locked device.
+AVIUM_FORCE_SET_FAKE_PROP ?= false
+
+# Blur Effect
+# The blur usually enabled on Android 16 QPR2.
+# If the blur not enabled, set to true to force enable blur for SystemUI.
+TARGET_FORCE_ENABLE_BLUR ?= true
